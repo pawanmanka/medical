18180bad2e67f79a58883283a016656ebd4c5ba2
chandreshsankhala <pawanmanka07@gmail.com>
2020-03-07 14:04:51 +0530

    fixes

--- resources/views/_slot.blade.php
+++ resources/views/_slot.blade.php
@@ -5,7 +5,7 @@
                 <input type="hidden" id="actual_fee_{{$index}}" name="actual_fee[{{$index}}]" value="{{$price}}">
                 <input type="hidden" id="discount_fee_{{$index}}" name="discount_fee[{{$index}}]"  value="{{$discount_fee}}">
                 <input type="hidden" id="availability_{{$index}}" name="availability[{{$index}}]"  value="{{$availability}}">
-                <h5 class="h5-sm steelblue-color">Time: {{$time}} <i data-index="{{$index}}" class="fa fa-pencil-alt  cursor edit_slot_item text-right"></i></h5>
+                <h5 class="h5-sm steelblue-color">Time: {{$time}} <i data-index="{{$index}}" class="fas fa-pencil-alt  cursor edit_slot_item text-right"></i></h5>
                 <p class="p-sm"> 
                     Fee: <spam>{{$price}}</spam>/-
                 </p>
